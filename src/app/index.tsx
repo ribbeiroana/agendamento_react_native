f4ec42f46424ec71775edc1c41320eea43d9ca72
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-09-23 15:34:11 -0300

    Arquitetura do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#008584' 
+      }}
+    >
+      <Text>Teste</Text>
+    </View>
+  );
+}
